6b1b1c29c2f0dbf730d9f379f6557633923f04e8
ohjinhokor <ohjinho7@gmail.com>
2022-07-15 13:28:53 +0900

    프로젝트 시작

---
+++ ehcache/src/main/java/example/ehcache/EhcacheApplication.java
@@ -0,0 +1,13 @@
+package example.ehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhcacheApplication.class, args);
+	}
+
+}
